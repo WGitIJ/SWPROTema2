6fa62ae81be4130221bbd8749674ba556db8bf29
WGitIJ <wisdomimade@paucasesnovescifp.cat>
2024-11-05 10:51:41 +0100

    vscode

--- SWPROTema2/src/volum1/numMajorTres.java
+++ SWPROTema2/src/volum1/numMajorTres.java
@@ -7,7 +7,16 @@ public class numMajorTres {
         int a = 4;
         int b = 3;
         int c = 9;
+        int max = 0;
+        int min;
 
-
+        if (a>b && a>c){
+            max = a;
+        } else if (b>a && b>c) {
+            max = b;
+        } else if (c>a && c>b) {
+            max = c;
+        }
+        System.out.println(max);
     }
 }
