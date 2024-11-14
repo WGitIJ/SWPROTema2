137c265e17f5edbbc2706b8a54b5ae9f42eed078
W631IJ <wimade631@alumnes.politecnicllevant.cat>
2024-11-14 17:06:04 +0100

    Prueba

---
+++ SWPROTema2/src/volum4/comparacionArrays.java
@@ -0,0 +1,31 @@
+package volum4;
+
+public class comparacionArrays  {
+    public static void main(String[] args) {
+        int[] arrayA = new int[10];
+        int[] arrayB = new int[10];
+        boolean sonIguales;
+
+        for (int i = 0; i < arrayA.length; i++) {
+            arrayA[i] = i;
+            System.out.println(arrayA[i]);
+        }
+        System.out.println();
+        for (int i = 0; i < arrayB.length; i++) {
+            arrayB[i] = i;
+            System.out.println(arrayB[i]);
+        }
+
+        if (arrayA == arrayB){
+            sonIguales = true;
+        } else{
+            sonIguales = false;
+        }
+        System.out.println("¿Son iguales? ");
+        System.out.println(sonIguales);
+    }
+    //? Podemos ver que no son iguales, aunque tengan el mismo tamaño y el mismo valor, no ocupan el mismo sitio en la
+    //? memoria, ya que la comparación compara dos variables ubicadas en distintas puntas de la memoria
+
+
+}
