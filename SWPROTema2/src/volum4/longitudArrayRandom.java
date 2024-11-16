94a069b7d8f7cc04c35566087b5e003fb75c2d9b
WGitIJ <wisdomimade@paucasesnovescifp.cat>
2024-11-16 10:39:04 +0100

    Implementacion de array de tamaño aleatorio

---
+++ SWPROTema2/src/volum4/longitudArrayRandom.java
@@ -0,0 +1,23 @@
+//* Escriu un altre classe que generi aleatòriament la longitud de dos arrays(mínim 2). Crea els arrays d'aquestes
+//* longituds. Els valors poden ser seqüencials o aleatoris, com vulguis. Compara-los. Evidentment si no tenen la
+//* mateixa longitud no són iguals.
+
+package volum4;
+
+public class longitudArrayRandom {
+    public static void main(String[] args) {
+        int numAleatorio = 2+(int)(Math.random()+1);
+        int [] array = new int[numAleatorio];
+        numAleatorio = 2+(int)(Math.random()+1);
+        int [] arrayB = new int[numAleatorio];
+        boolean sonIguales;
+
+        if (arrayB == array){
+            sonIguales = true;
+        } else{
+            sonIguales = false;
+        }
+
+        System.out.println("¿Array y ArrayB son iguales? " + sonIguales);
+    }
+}
