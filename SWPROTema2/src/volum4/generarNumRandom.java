06333628a650aa904276f8440f03e338157c4c0c
WGitIJ <wisdomimade@paucasesnovescifp.cat>
2024-11-19 10:33:03 +0100

    Empezando el volumen 5

--- SWPROTema2/src/volum4/generarNumRandom.java
+++ SWPROTema2/src/volum4/generarNumRandom.java
@@ -1,21 +1,26 @@
 //* (Opcional) Genera sencers aleatòris entre un valor màxim i un valor mínim fins que els hagis generats tots. Mostra
 //* els valors generats i quantes iteracions has necessitat per conseguir-ho.
 
-package volum3;
+package volum4;
+
+import java.util.Arrays;
 
 public class generarNumRandom {
     public static void main(String[] args) {
         int max = 5;
         int min = 0;
+        int[] array = new int[max+1];
         int contador = 0;
         int numAleatorio = (int) (Math.random()*((max+min)+1));
         for (int i = 0; i <= max;i++) {
             while (numAleatorio != i){
                 contador++;
                 numAleatorio = (int) (Math.random()*((max+min)+1));
+                array[i] = numAleatorio;
             }
-            System.out.println(numAleatorio);
         }
+        System.out.println(Arrays.toString(array));
         System.out.println("Intentos: "+ contador);
+
     }
 }
---
+++ SWPROTema2/src/volume5/Ejercicio_39.java
@@ -0,0 +1,13 @@
+package volume5;
+
+import java.util.Scanner;
+
+public class Ejercicio_39 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("¿Cuantos valores quieres?");
+        int valoresUsurio = sc.nextInt();
+
+        int[] arrayUsuario = new int[valoresUsurio];
+    }
+}
